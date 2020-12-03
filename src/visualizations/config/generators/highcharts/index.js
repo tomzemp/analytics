a83645540619ec6631e4033f9ea568cda4fd19a5
Jen Jones Arnesen <jennifer@dhis2.org>
2020-12-03 11:29:38 +0100

    feat: enable fullscreen feature for highcharts

--- src/visualizations/config/generators/highcharts/index.js
+++ src/visualizations/config/generators/highcharts/index.js
@@ -4,6 +4,7 @@ import HSG from 'highcharts/modules/solid-gauge'
 import HNDTD from 'highcharts/modules/no-data-to-display'
 import HE from 'highcharts/modules/exporting'
 import HPF from 'highcharts/modules/pattern-fill'
+import HFS from 'highcharts/modules/full-screen'
 
 // apply
 HM(H)
@@ -11,6 +12,7 @@ HSG(H)
 HNDTD(H)
 HE(H)
 HPF(H)
+HFS(H)
 
 export default function (config, el) {
     if (config) {
